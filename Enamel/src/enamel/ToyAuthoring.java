f7f0e2f8b9f7bb325c12bddaeb2d4ec2138f735b
GianlucaCorvinelli <corv9@my.yorku.ca>
2018-01-22 11:22:04 -0500

    testing conflict

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -4,6 +4,6 @@ public class ToyAuthoring {
 
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 2 + ".txt");
     }
 }
