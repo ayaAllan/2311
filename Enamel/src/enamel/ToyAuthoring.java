803e30930a74520cf4a2db80043f116240b4db44
GianlucaCorvinelli <corv9@my.yorku.ca>
2018-01-22 08:58:21 -0500

    second test for branching

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -27,5 +27,7 @@ public class ToyAuthoring {
 
 		ScenarioParser s = new ScenarioParser(true);
 		s.setScenarioFile("FactoryScenarios/" + chooser.getSelectedFile().getName());
+		
+		//this is a second test for branching in the master branch
 	}
 }
