fa2b2217cc352ca69a9e221273bdc9241b86f13b
GianlucaCorvinelli <corv9@my.yorku.ca>
2018-02-02 13:59:50 -0500

    added the initial setup menu when create scenario button is called

--- Enamel/src/enamel/MainMenuControllerAA.java
+++ Enamel/src/enamel/MainMenuControllerAA.java
@@ -2,14 +2,21 @@ package enamel;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControllerAuthoringApp implements Initializable {
+public class MainMenuControllerAA implements Initializable {
 
 	ObservableList<String> readSpeedList = FXCollections.observableArrayList("Very Fast", "Fast", "Medium", "Slow",
 			"Very Slow", "Beginner");
@@ -23,6 +30,19 @@ public class ControllerAuthoringApp implements Initializable {
 		System.out.println("This is a test");
 		readSpeedBox.setValue("Beginner");
 		readSpeedBox.setItems(readSpeedList);
+
+	}
+
+	/*
+	 * Brings the user to creation setup
+	 */
+	public void createScenarioButton(ActionEvent event) throws IOException {
+		Parent creationSetupParent = FXMLLoader.load(getClass().getResource("CreationSetupView.fxml"));
+		Scene creationSetupScene = new Scene(creationSetupParent);
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(creationSetupScene);
+		window.show();
 
 	}
 
