c52d7ca2a18ccbdadb8737f08bc30631d24ffeb8
Mark <mark.sav@hotmail.com>
2018-02-10 16:17:35 -0500

    Tests for the Scene Class

--- Enamel/src/enamel/SceneTest.java
+++ Enamel/src/enamel/SceneTest.java
@@ -2,13 +2,74 @@ package enamel;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class SceneTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test01_ConstructorGetQuestion() {
+		final String questionConstant = "What letter do the pins display?";
+		Scene testScene = new Scene("", questionConstant, null, null);
+		assertEquals(questionConstant, testScene.getQuestion());
 	}
-
+	@Test
+	public void test02_setQuestion() {
+		final String question = "What letter do the pins display?";
+		Scene testScene = new Scene(null, "", null, null);
+		assertEquals("", testScene.getQuestion());
+		testScene.setQuestion(question);
+		assertEquals(question, testScene.getQuestion());
+	}
+	
+	@Test
+	public void test03_getSceneName() {
+		final String sceneName = "Scene 1";
+		Scene testScene = new Scene("", null, null, null);
+		assertEquals("", testScene.getSceneName());
+		testScene.setSceneName(sceneName);
+		assertEquals(sceneName, testScene.getSceneName());
+	}
+	
+	@Test
+	public void test04_getButtonsNull() {
+		Scene testScene = new Scene(null, null, null, null);
+		assertEquals(null, testScene.getButtons());
+	}
+	
+	@Test
+	public void test05_getButtonsEmpty() {
+		List<Integer> buttons = new ArrayList<>();
+		Scene testScene = new Scene(null, null, null, buttons);
+		assertEquals(buttons, testScene.getButtons());
+	}
+	@Test
+	public void test06_getButtonsNonEmpty() {
+		List<Integer> buttons = Arrays.asList(1,2,3,4,5);
+		Scene testScene = new Scene(null, null, null, buttons);
+		assertEquals(buttons, testScene.getButtons());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
