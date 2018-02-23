9f73966d54ca19877a2851f8c6facb700db63d03
GianlucaCorvinelli <corv9@my.yorku.ca>
2018-02-23 16:38:21 -0500

    added Mark's test cases

--- Enamel/src/enamel/ScenarioAATest.java
+++ Enamel/src/enamel/ScenarioAATest.java
@@ -4,11 +4,69 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Mark Savin
+ *
+ */
+
 public class ScenarioAATest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test01_getNOB() {
+		ScenarioAA nob = new ScenarioAA(null, 1, 1);
+		// nob.setNumberOfButtons(1);
+		int numberOfButtons = 1;
+		assertEquals(nob.getNOB(), numberOfButtons);
 	}
 
+	@Test
+	public void test02_getNOC() {
+		ScenarioAA noc = new ScenarioAA(null, 1, 1);
+		// noc.setNumberOfCells(1);;
+		int numberOfCells = 1;
+		assertEquals(noc.getNOC(), numberOfCells);
+	}
+
+	@Test
+	public void test03_getScenarioName() {
+		ScenarioAA scenarioName = new ScenarioAA("Scenario Name", 1, 1);
+		scenarioName.setScenarioName("Scenario Name");
+		assertEquals(scenarioName.getScenarioName(), "Scenario Name");
+	}
+
+	@Test
+	public void test04_getCurrentScene() {
+		// SceneAA currentScene = new SceneAA(1, 1);
+		// currentScene.setName("Scene1");
+		// assertEquals(currentScene.getSceneName(), "Scene1");
+	}
+
+	@Test
+	public void test05_getScenario() {
+		ScenarioAA scenario = new ScenarioAA("", 1, 1);
+		scenario.setScenarioName("");
+		assertEquals(scenario.getScenarioName(), "");
+	}
+
+	@Test
+	public void test07_setNumberOfButtons() {
+		ScenarioAA nob = new ScenarioAA(null, 1, 1);
+		int numberOfButtons = 1;
+		nob.setNumberOfButtons(numberOfButtons);
+		assertEquals(nob.getNOB(), numberOfButtons);
+	}
+
+	@Test
+	public void test08_setNumberOfCells() {
+		ScenarioAA noc = new ScenarioAA(null, 1, 1);
+		int numberOfCells = 1;
+		noc.setNumberOfCells(numberOfCells);
+		assertEquals(noc.getNOC(), numberOfCells);
+	}
+
+	@Test
+	public void test09_setCurrentSceneIndex() {
+		SceneAA currentSceneIndex = new SceneAA(1, 1);
+	}
 }
--- Enamel/src/enamel/SceneAATest.java
+++ Enamel/src/enamel/SceneAATest.java
@@ -8,25 +8,32 @@ import java.util.List;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 public class SceneAATest {
 
 	// @Mark im sorry but I changed the constructor for the app so all the test
-	// cases were failing, methods can be tested now as well as the constructor. Try
-	// to group the 'one line of code' getters together and the 'one line of code'
-	// setters also in a seperate test case together. All the other methods should
+	// cases were failing, methods can be tested now as well as the constructor.
+	// Try
+	// to group the 'one line of code' getters together and the 'one line of
+	// code'
+	// setters also in a seperate test case together. All the other methods
+	// should
 	// have their own test cases (at least one)
-	@Test
-	public void test01_ConstructorGetQuestion() {
-		final String questionConstant = "What letter do the pins display?";
 
+	@Test
+	public void test02_getSceneName() {
+		// final String sceneName = "Scene1";
 	}
 
 	@Test
-	public void test02_setQuestion() {
-		final String question = "What letter do the pins display?";
+	public void test03_setNametest() {
+		// final String sceneName = "Lesson 1";
+	}
 
+	@Test
+	public void test07_getNOB() {
+		int nob = 8;
+		SceneAA noble = new SceneAA(0, 8);
+		assertEquals(nob, noble.getNOB());
 	}
 
 	@Test
@@ -37,23 +44,95 @@ public class SceneAATest {
 
 	@Test
 	public void test04_getButtonsNull() {
-
+		List<Integer> buttons = new ArrayList<>();
 	}
 
 	@Test
 	public void test05_getButtonsEmpty() {
 		List<Integer> buttons = new ArrayList<>();
-
 	}
 
 	@Test
 	public void test06_getButtonsNonEmpty() {
 		List<Integer> buttons = Arrays.asList(1, 2, 3, 4, 5);
+	}
 
+	@Test
+	public void test09_getSceneName() {
+		SceneAA sceneName = new SceneAA(1, 1);
+		String name = "name";
+		sceneName.setName(name);
+		assertEquals(name, sceneName.getSceneName());
+	}
+
+	@Test
+	public void test08_setQuestion() {
+		SceneAA scene = new SceneAA(1, 2);
+		String question = "hgh";
+		scene.setQuestion(question);
+		assertEquals(question, scene.getQuestion());
+	}
+
+	@Test
+	public void setPinsScene() {
+		SceneAA pin = new SceneAA(1, 1);
+		pin.getBrailleCells().get(0).setPins("10000001");
+		assertEquals(pin.getBrailleCells().get(0).getPinState(0), true);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(1), false);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(2), false);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(3), false);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(4), false);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(5), false);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(6), false);
+		assertEquals(pin.getBrailleCells().get(0).getPinState(7), true);
+	}
+
+	@Test
+	public void test10_clearPins() {
+		SceneAA scene = new SceneAA(3, 1);
+		scene.getBrailleCells().get(0).setPins("10101010");
+		assertEquals(scene.getBrailleCells().get(0).getPinState(0), true);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(1), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(2), true);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(3), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(4), true);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(5), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(6), true);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(7), false);
+		scene.clearPins(0);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(0), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(1), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(2), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(3), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(4), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(5), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(6), false);
+		assertEquals(scene.getBrailleCells().get(0).getPinState(7), false);
+		scene.getBrailleCells().get(1).setPins("10111011");
+		scene.clearPins(1);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(0), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(1), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(2), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(3), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(4), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(5), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(6), false);
+		assertEquals(scene.getBrailleCells().get(1).getPinState(7), false);
+		scene.getBrailleCells().get(2).setPins("10101111");
+		scene.clearPins(2);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(0), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(1), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(2), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(3), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(4), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(5), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(6), false);
+		assertEquals(scene.getBrailleCells().get(2).getPinState(7), false);
 	}
 
 	// testing if the pins correctly transfer from binary to boolean
-	// could do all the letters which is just copy and paste but thats way to much
+	// could do all the letters which is just copy and paste but thats way to
+	// much
 	// code for no reason
 	@Test
 	public void getPinsAsBooleanTest() throws InterruptedException {
@@ -100,4 +179,45 @@ public class SceneAATest {
 		assertEquals(listAsBoolean3, s.getPinsAsBoolean(bCell1));
 	}
 
+	@Test
+	public void setInteractionPreset() {
+		SceneAA interaction = new SceneAA(1, 4);
+		interaction.setInteractionPreset(1, "Play Correct Audio Clip");
+		assertEquals(interaction.getInteractionPreset().get(1), "Play Correct Audio Clip");
+	}
+
+	@Test
+	public void setInterationTextInput() {
+		SceneAA textInput = new SceneAA(1, 1);
+		textInput.setInteractionTextInput(1, "No Audio File");
+		assertEquals(textInput.getInteractionTextInput().get(1), "No Audio File");
+	}
+
+	@Test
+	public void getButtons() {
+		SceneAA buttons = new SceneAA(1, 1);
+		List<Integer> Buttons = Arrays.asList();
+		assertEquals(buttons.getButtons(), Buttons);
+	}
+
+	@Test
+	public void getAudioNameOption1() {
+		SceneAA audioName = new SceneAA(1, 1);
+		String audioNameOption1 = "Audio1";
+		audioName.setAudioNameOption1(audioNameOption1);
+		assertEquals(audioName.getAudioNameOption1(), "Audio1");
+	}
+
+	@Test
+	public void getInteractionAudioNameAtIndex() {
+		SceneAA audioName = new SceneAA(1, 1);
+		audioName.setInteractionAudioNameAtIndex("Audio1", 1);
+		assertEquals(audioName.getAudioNameOption1(), "Audio1");
+	}
+
+	@Test
+	public void audioOption1Exists() {
+		SceneAA option1 = new SceneAA(1, 1);
+		option1.audioOption1Exists().booleanValue();
+	}
 }
